3dceda1ce13e06709c69423d794cce814cbb3913
Marvellous O. Oyebanjo <88282876+Soullevram@users.noreply.github.com>
2025-07-23 17:05:44 +0100

    Update ExomeDepthScript.R

--- modules/local/ExomeDepth/ExomeDepthScript.R
+++ modules/local/ExomeDepth/ExomeDepthScript.R
@@ -11,22 +11,24 @@ suppressPackageStartupMessages({
 # CLI argument parser
 # -----------------------------
 option_list <- list(
-  make_option("--bam", type = "character", help = "Input BAM file"),
+  make_option("--bam", type = "character", help = "Test BAM file"),
+  make_option("--controls", type = "character", help = "Comma-separated list of control BAM files"),
   make_option("--bed", type = "character", help = "BED file with target regions"),
   make_option("--fasta", type = "character", help = "Reference FASTA file", default = NULL),
   make_option("--out", type = "character", help = "Output file for CNV calls"),
   make_option("--readlen", type = "integer", default = 150, help = "Read length (default: 150)"),
-  make_option("--transition_prob", type = "double", default = 1e-4, help = "Transition probability for CNV calling"),
-  make_option("--expected_cnv_len", type = "double", default = 1e6, help = "Expected CNV length in bp")
+  make_option("--transition_prob", type = "double", default = 1e-4),
+  make_option("--expected_cnv_len", type = "double", default = 1e6),
+  make_option("--n_bins_reduced", type = "integer", default = 10000),
+  make_option("--phi_bins", type = "double", default = 1)
 )
 
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
 # -----------------------------
-# Load BED and add dummy gene column if missing
+# Load BED
 # -----------------------------
-cat("Loading BED file:", opt$bed, "\n")
 bed_df <- read.table(opt$bed, sep = "\t", header = FALSE, stringsAsFactors = FALSE)
 colnames(bed_df) <- c("chr", "start", "end", "gene")[1:ncol(bed_df)]
 if (!"gene" %in% colnames(bed_df)) {
@@ -34,44 +36,70 @@ if (!"gene" %in% colnames(bed_df)) {
 }
 
 # -----------------------------
-# Count reads from BAM
+# Count reads from BAMs
 # -----------------------------
-cat("Reading BAM:", opt$bam, "\n")
-bam_counts <- getBamCounts(
+test_bam <- opt$bam
+control_bams <- strsplit(opt$controls, ",")[[1]]
+
+bam_files <- c(test_bam, control_bams)
+
+counts <- getBamCounts(
   bed.frame = bed_df,
-  bam.files = opt$bam,
+  bam.files = bam_files,
   include.chr = TRUE,
   read.width = opt$readlen,
   referenceFasta = opt$fasta
 )
 
-count_df <- as.data.frame(bam_counts)
-colnames(count_df)[which(names(count_df) == "reads")] <- "test"
-
-# Use the test sample as its own reference (mock)
-count_df$reference <- count_df$test
+counts_df <- as.data.frame(counts)
+colnames(counts_df) <- gsub(".bam", "", colnames(counts_df))
 
 # -----------------------------
-# ExomeDepth CNV calling
+# Split counts into test + controls
 # -----------------------------
-cat("Running ExomeDepth\n")
+test_sample <- gsub(".bam", "", basename(test_bam))
+control_samples <- gsub(".bam", "", basename(control_bams))
+
+test_counts <- as.matrix(counts_df[, test_sample, drop = FALSE])
+control_counts <- as.matrix(counts_df[, control_samples, drop = FALSE])
+
+# -----------------------------
+# Select reference set
+# -----------------------------
+references <- select.reference.set(
+  test.counts = test_counts,
+  reference.count = control_counts,
+  bin.length = (counts_df$end - counts_df$start) / 1000,
+  n.bins.reduced = opt$n_bins_reduced,
+  phi.bins = opt$phi_bins
+)
+
+selected_controls <- apply(
+  X = as.matrix(control_counts[, references$reference.choice]),
+  MARGIN = 1,
+  FUN = sum
+)
+
+# -----------------------------
+# Run ExomeDepth
+# -----------------------------
 exome <- new('ExomeDepth',
-             test = count_df$test,
-             reference = count_df$reference,
+             test = test_counts,
+             reference = selected_controls,
              formula = 'cbind(test, reference) ~ 1')
 
 cnvs <- CallCNVs(
   x = exome,
   transition.probability = opt$transition_prob,
   expected.CNV.length = opt$expected_cnv_len,
-  chromosome = count_df$space,
-  start = count_df$start,
-  end = count_df$end,
-  name = count_df$names
+  chromosome = counts_df$space,
+  start = counts_df$start,
+  end = counts_df$end,
+  name = counts_df$names
 )
 
 # -----------------------------
-# Add gene annotation
+# Annotate gene names
 # -----------------------------
 getGeneForPosition <- function(bedData, chr, pos) {
   gene <- bedData[bedData$chr == chr & pos >= bedData$start & pos <= bedData$end, "gene"]
@@ -81,11 +109,11 @@ getGeneForPosition <- function(bedData, chr, pos) {
 
 if (nrow(cnvs) > 0) {
   cnvs$gene <- mapply(getGeneForPosition, chr = cnvs$chromosome, pos = cnvs$end, MoreArgs = list(bedData = bed_df))
+  cnvs$sample <- test_sample
 }
 
 # -----------------------------
-# Save results
+# Output
 # -----------------------------
-cat("Saving CNV results to", opt$out, "\n")
 write.table(cnvs, file = opt$out, sep = "\t", row.names = FALSE, quote = FALSE)
-cat("Finished.\n")
+cat("Finished CNV calling for", test_sample, "\n")
